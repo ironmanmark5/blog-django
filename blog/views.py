c146994f0409622f7f1ea5e9dcde16e6e8bf8436
ironmanmark5 <138386456+ironmanmark5@users.noreply.github.com>
2025-02-28 12:35:36 -0300

    Template! (Correção)

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def  post_list(request):
-    return render(request, "blog/post_list.html," {})
+    return render(request, "blog/post_list.html,")
